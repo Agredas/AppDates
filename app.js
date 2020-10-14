daa3c4b29b5569678412473ca995991a25684110
Agredas <andreagredas@gmail.com>
2020-10-14 23:05:15 +0200

    Login and Logout done.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // Basic Import Section
 const express = require('express');
 const app = express();
+const auth = require('./middleware/auth');
 
 //Middleware
 app.use(express.json());
 
 // Modular Imports
 const {register} =  require('./controllers/clientController');
+const {login} = require('./controllers/clientController');
+const {logout} = require('./controllers/clientController');
 const {showClients} =  require('./controllers/clientController');
 
 // DB Connection
@@ -17,6 +20,8 @@ dbconnect();
 //Client actions
 app.get('/client/showAll', showClients);
 app.post('/client/register', register);
+app.post('/client/login', login);
+app.post('/client/logout', logout);
 
 // Port Listen
 app.listen(3000, () => console.log('Server running.'))
--- controllers/clientController.js
+++ controllers/clientController.js
@@ -8,6 +8,50 @@ const register = (req,res) => {
   .catch(error => console.log('There was a problem trying to register the client.' + error))
 }
 
+const login = async(req, res) => {
+  try {
+    const client = await Client.findOne({
+      where:{
+        email: req.body.email
+      }
+    })
+    if(!client) {
+      return res.status(400).send({
+        message: 'Wrong credentials.'
+      })
+    }
+    const isMatch = await bcrypt.compare(req.body.password, client.password)
+    if (!isMatch){
+      return res.status(400).send({
+        message: 'Wrong credentials.'
+      })
+    }
+    const token = jwt.sign({id: client.id }, 'elvistecmec', { expiresIn: '30d' })
+    console.log(token)
+    client.token = token;
+    await client.save()
+    res.send(client);
+  }catch (error){
+    console.error(error);
+    res.status(500).send({message: 'There was a problem trying to log in.'})
+  }
+}
+
+const logout = async(req,res) => {
+  try {
+    const outClient = await ClientModel.findOne({token:token});
+    outClient.token = null;
+    outClient.save();
+    res.send('See you soon.')
+
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({message: 'There was a problem trying to log out.'})
+  }
+}
+
+
+
 const showClients = (req,res) => {
   new ClientModel.find({})
   .then(clients =>{
@@ -18,5 +62,7 @@ const showClients = (req,res) => {
 
 module.exports = {
   register,
+  login,
+  logout,
   showClients
 }
