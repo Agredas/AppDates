c8604a6c6cdb9b60711a7cd495be9f04bdb6ab44
Agredas <andreagredas@gmail.com>
2020-10-14 17:46:10 +0200

    Client actions added to app.js

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ const app = express();
 app.use(express.json());
 
 // Modular Imports
-
+const {register} =  require('./controllers/clientController');
+const {showClients} =  require('./controllers/clientController');
 
 // DB Connection
 const dbconnect = require('./config/dbconnect');
 dbconnect();
+
+//ACTIONS
+//Client actions
+app.get('/client/showAll', showClients);
+app.post('/client/register', register);
 
 // Port Listen
 app.listen(3000, () => console.log('Server running.'))
