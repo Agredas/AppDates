8ad920378272735ad0cb06fc5bfe43e4d7a64929
Agredas <andreagredas@gmail.com>
2020-10-14 20:01:47 +0200

    Middleware and JsonWebToken added.

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const jwt = require('jsonwebtoken');
+const {
+  Client
+} = require('../models');
+
+const auth = async(req,res,next) => {
+  try{
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token,'elvistecmec');
+    const client = await Client.findOne({
+      where: {token:token}
+    });
+    console.log(client)
+    if(!client){
+      return res.status(401).send({message: 'You are not authorized.'})
+    }
+    req.client = client;
+    next();
+  } catch (error){
+    console.error(error)
+    res.status(401).send({error, message: 'You are not authorized.'})
+  }
+}
