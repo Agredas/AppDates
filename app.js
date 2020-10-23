a90542dcddebbad64f2b82df21cf078b26eae0d9
Agredas <andreagredas@gmail.com>
2020-10-23 19:26:12 +0200

    Adding Cors to app.js

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ const AppointmentsRouter = require('./routers/appointmentRouter');
 const PORT = process.env.PORT || 3000;
 //Middleware
 app.use(express.json());
-app.use(cors);
+/* app.use(cors); */
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 
 // DB Connection
